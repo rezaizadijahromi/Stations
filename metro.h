82cd6fb3168ac869556220e23fae4c5b8d2d60c5
reza izadi <rezaizadij2000@gmail.com>
2025-08-12 15:48:21 +0200

    add: layout for line functions

--- metro.h
+++ metro.h
@@ -9,6 +9,10 @@
 #define STATION_NAME_LEN 40
 #define STATIONS_MAX 5000
 
+#define LINE_MAGIC 0x314E494Cu
+#define LINE_VER 1u
+#define LINE_MAX_STOPS 5000
+
 typedef struct
 {
     uint32_t magic;
@@ -19,12 +23,31 @@ typedef struct
 
 typedef struct
 {
+    uint32_t magic;    // LINE_MAGIC
+    uint16_t version;  // LINE_VER
+    uint16_t reserved; // 0
+    uint32_t count;    // number of LineStopRec that follow
+} LineFileHeader;
+
+typedef struct
+{
     char id[STATION_ID_LEN];
     char name[STATION_NAME_LEN];
 } StationRec;
 
+typedef struct
+{
+    char station_id[STATION_ID_LEN];
+    uint16_t travel_to_next_min;
+    uint16_t dwell_ming;
+} LineStopRec;
+
 int load_stations(const char *filename, StationRec *out, int max_count, int *loaded_count);
 int append_station(const char *filename, const char *id, const char *name);
 void copy_padded(char dst[], size_t dst_len, const char *src);
+
+int load_line(const char *filename, LineStopRec *out, int max_count, int *loaded_count);
+int append_line_stop(const char *filename, const char *id, const char *name);
+int find_station_index(const StationRec *stations, int station_count, const char *id);
 
 #endif
