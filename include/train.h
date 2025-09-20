de75ce7c2d52950e44100e0fefd6062f0c0fd4cf
reza izadi jahromi <rezaizadij2000@gmail.com>
2025-09-20 17:06:58 +0200

    add tasks in the train and now can have multiple train

--- include/train.h
+++ include/train.h
@@ -10,7 +10,16 @@ typedef struct
     size_t index;
 } Train;
 
-static inline void train_start(Train *t, uint16_t id)
+typedef struct
+{
+    Train *t;
+    const Station *route;
+    size_t n; // Number of stations
+    int *done_flag;
+} TrainCtx;
+
+static inline void
+train_start(Train *t, uint16_t id)
 {
     t->id = id;
     t->index = 0;
@@ -23,3 +32,27 @@ static inline const Station *train_current(const Train *t, const Station *rout, 
 
 int train_move_next(Train *t, size_t n);
 void train_run_route(Train *t, const Station *rout, size_t n, unsigned seconds);
+
+// Tasks
+void train_task(void *ctx_v)
+{
+    TrainCtx *ctx = (TrainCtx *)ctx_v;
+
+    if (ctx->t->index >= ctx->n)
+    {
+        *(ctx->done_flag) = 1;
+        return;
+    }
+
+    const Station *s = &ctx->route[ctx->t->index];
+    printf("Train %u at: %s (station id=%u)\n", (unsigned)ctx->t->id, s->station_name, (unsigned)s->id);
+
+    ctx->t->index++;
+
+    if (ctx->t->index >= ctx->n)
+    {
+        *(ctx->done_flag) = 1;
+    }
+}
+
+int stop_when_done(void *p) { return *(int *)p; }
